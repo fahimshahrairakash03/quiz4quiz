3f2efd75f5f7f06acf9580dad76c4b9cce265820
Fahim Shahriar Akash <fahimshahriarakash03@gmail.com>
2023-12-16 14:37:54 +0600

    answer organized

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -4,13 +4,25 @@ import Options from "../Options/Options";
 const Questions = ({ ques }) => {
   console.log(ques);
   const { correctAnswer, id, options, question } = ques;
+  console.log(ques);
+  const handleAnswer = (op) => {
+    if (op === correctAnswer) {
+      console.log("correct Answer");
+    } else {
+      console.log("Incorrect Answer");
+    }
+  };
 
   return (
     <div className="pt-5">
       <h4 className="text-center p-5">{question}</h4>
       <div className="d-flex flex-column align-items-center w-100">
         {options.map((option) => (
-          <Options option={option}></Options>
+          <Options
+            key={id}
+            option={option}
+            handleAnswer={handleAnswer}
+          ></Options>
         ))}
       </div>
     </div>
